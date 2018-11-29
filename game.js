29ca368bf2614c89808e9b574de824b9d79d04fc
Ian Gornall <ian.gornall@gmail.com>
2018-11-29 18:31:55 -0500

    Added enemy with movement back and forth.

--- game.js
+++ game.js
@@ -2,12 +2,18 @@ let rectangle2;
 let hero;
 let cursors;
 let shurikens;
+let enemy1Path;
+let enemy;
 let preload = function() {
   this.load.atlas('ninja', 'assets/ninja.png', 'assets/ninja-array.json');
+  this.load.atlas('zombieGirl', 'assets/zombie-girl.png', 'assets/zombie-girl.json');
+  this.load.atlas('zombieBoy', 'assets/zombie-boy.png', 'assets/zombie-boy.json');
   this.load.image('shuriken', 'assets/shuriken.png');
   this.load.image('tileset', 'assets/tileset.png');
   this.load.image('objects', 'assets/objects.png');
   this.load.tilemapTiledJSON('map1', 'assets/level1.json');
+  
+
 }
 let create = function() {
   let map = this.make.tilemap({key: 'map1', tileWidth: 128, tileHeight: 128});
@@ -20,9 +26,14 @@ let create = function() {
   let mainLayer2 = map.createStaticLayer('main2', objects, 0, 0);
   mainLayer.setCollisionByProperty({collides: true});
   mainLayer2.setCollisionByProperty({collides: true});
-  hero = this.physics.add.sprite(100, 100, 'ninja');
+  let spawnPoint = map.findObject("objects", obj => obj.name === "spawn");
+  console.log(spawnPoint);
+  hero = this.physics.add.sprite(spawnPoint.x, spawnPoint.y, 'ninja');
   hero.setScale(0.3);
   hero.setBounce(0.2);
+  enemy1Path = map.findObject("objects", obj => obj.name === "enemy1");
+  enemy = this.physics.add.sprite(enemy1Path.x + enemy1Path.width, enemy1Path.y, 'zombieGirl');
+  enemy.y -= enemy.height
   let camera = this.cameras.main;
   camera.setBounds(0, 0, map.widthInPixels, map.heightInPixels);
   this.physics.world.setBounds(0, 0, map.widthInPixels, map.heightInPixels, true, true, true, true);
@@ -30,7 +41,10 @@ let create = function() {
   this.physics.add.collider(hero, mainLayer);
   this.physics.add.collider(hero, mainLayer2);
   hero.setCollideWorldBounds(true);
-  console.log(map.widthInPixels, map.heightInPixels);
+  this.physics.add.collider(enemy, mainLayer);
+  this.physics.add.collider(enemy, mainLayer2);
+  enemy.setCollideWorldBounds(true);
+  this.physics.add.overlap(hero, enemy);
   this.anims.create({
     key: 'run',
     frames: this.anims.generateFrameNames('ninja', {
@@ -59,11 +73,49 @@ let create = function() {
     repeat: -1
   });
   this.anims.create({
-    key: 'throw',
-  })
+    key: 'zombieGirlWalk',
+    frames: this.anims.generateFrameNames('zombieGirl', {
+      prefix: 'Walk',
+      start: 1, end: 10, zeroPad: 2
+    }),
+    frameRate: 10,
+    repeat: -1
+  });
+  this.anims.create({
+    key: 'zombieGirlAttack',
+    frames: this.anims.generateFrameNames('zombieGirl', {
+      prefix: 'Attack',
+      start: 1, end: 8, zeroPad: 2
+    }),
+    frameRate: 10,
+    repeat: -1
+  });
+  this.anims.create({
+    key: 'zombieGirlDead',
+    frames: this.anims.generateFrameNames('zombieGirl', {
+      prefix: 'Dead',
+      start: 1, end: 12, zeroPad: 2
+    }),
+    frameRate: 10,
+    repeat: -1
+  });
   cursors = this.input.keyboard.createCursorKeys();
+  enemy.setSize(enemy.width / 1.5, enemy.height / 1.05);
+  enemy.setOffset(enemy.body.offset.x, enemy.body.offset.y + enemy.height / 20);
+  enemy.setVelocityX(-200);
+  enemy.anims.play('zombieGirlWalk', true);
+  enemy.flipX = true;
+  console.log(enemy.x, enemy.width, enemy1Path.x, enemy1Path.width);
 }
 let update = function() {
+  if (enemy.x <= enemy1Path.x) {
+    enemy.setVelocityX(200);
+    enemy.flipX = false;
+  }
+  if (enemy.x >= enemy1Path.x + enemy1Path.width) {
+    enemy.setVelocityX(-200);
+    enemy.flipX = true;
+  }
   if (cursors.left.isDown){
     hero.setVelocityX(-600);
     hero.flipX = true;    
@@ -82,7 +134,6 @@ let update = function() {
   }
   if (Phaser.Input.Keyboard.JustDown(cursors.space))  {
     console.log(hero);
-
     let shuriken = this.physics.add.sprite(hero.flipX ? hero.x - 50 : hero.x + 50 / 2, hero.y, 'shuriken');
     shuriken.setScale(0.05);
     shuriken.body.setAllowGravity(false);
@@ -98,7 +149,8 @@ let config = {
     default: 'arcade',
     arcade: {
         gravity: { y: 3000 },
-        debug: false,
+        debug: true,
+        overlapBias: 16,
         tileBias: 64
     }
   },
